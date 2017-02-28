99b16c239c8f59503746e8874ca7c420718d2a6f
Wanasak <u510610433@gmail.com>
2017-02-28 15:20:58 +0700

    apply attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { HighlightDirective } from './hightlight.directive';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HighlightDirective
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
